d4b207e8a4e35751a9b44f09ae485dce625427d5
Ustym <ystumys@ukr.net>
2022-04-12 19:46:20 +0300

    added full adaptation for control block

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,46 +2,67 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     
+
+    //Hamburger
+
+
     const hamburger = document.querySelector('.hamburger'),
           menu = document.querySelector('.control__menu'),
           menuBtnClose = document.querySelector('.control__menu_close');
     
     hamburger.addEventListener('click', () => {
         menu.classList.add('active');
+        openOverlay();
+        //document.body.style.overflow = 'hidden';
     });
 
     function closeMenu() {
         menu.classList.remove('active');
+        closeOverlay();
+        //document.body.style.overflow = '';
     }
     
     menuBtnClose.addEventListener('click', closeMenu);
 
-    menu.addEventListener('click', (e) => {
-        if(e.target === menu) {
-            closeMenu();
-        }
-    });
-    
-    const countries = document.querySelector('.countries'),
-          countriesBtnOpen = document.querySelector('.control__btn_open'),
-          countriesMenu = document.querySelector('.countries__menu'),
-          countriesBtnClose = document.querySelector('.countries__menu_close');
+    //Countries
+
+
+    const countriesBtnOpen = document.querySelector('.control__btn_open'),
+          countriesMenu = document.querySelector('.countries'),
+          countriesBtnClose = document.querySelector('.countries__btn_close');
 
     countriesBtnOpen.addEventListener('click', () => {
         countriesMenu.classList.add('active');
+        openOverlay();
+        //document.body.style.overflow = 'hidden';
     });
 
     function closeCountries() {
         countriesMenu.classList.remove('active');
+        closeOverlay();
+        //document.body.style.overflow = '';
     }
 
     countriesBtnClose.addEventListener('click', closeCountries);
 
-    countriesMenu.addEventListener('click', (e) => {
-        if(e.target === countriesMenu) {
+
+    //Overlay
+
+    const overlay = document.querySelector('.overlay');
+
+    function openOverlay() {
+        overlay.classList.add('active');
+    }
+
+    function closeOverlay() {
+        overlay.classList.remove('active');
+    }
+
+    overlay.addEventListener('click', (e) => {
+        if(e.target === overlay) {
             closeCountries();
+            closeMenu();
         }
     });
-    
 });
 
